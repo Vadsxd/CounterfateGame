d1499cbb4bc4753f1bcbc427a0888f9d5bd7d01b
Vadsxd <vadim.sannikov.2018@mail.ru>
2025-03-18 13:04:05 +0300

    create new directory

---
+++ config.py
@@ -0,0 +1,16 @@
+# Размеры окна
+WIDTH, HEIGHT = 800, 600
+
+# Цвета
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Игрок
+PLAYER_SIZE = 40
+PLAYER_X = 200
+PLAYER_Y = 200
+PLAYER_SPEED = 5
+
+# Производительность
+FPS = 45
--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import sys
 
 import pygame
 
-from Hole import Hole
-from Player import Player
+import config
+from objects.Hole import Hole
+from objects.Player import Player
 
 # Инициализация pygame
 pygame.init()
 
 # Определение размеров окна
-WIDTH, HEIGHT = 800, 600
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
 pygame.display.set_caption("Game with a Hole")
 
-# Цвета
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-
 # Инициализация объектов
-player = Player(40, 200, 200, 5)
-hole = Hole(50, WIDTH // 2, HEIGHT // 2)
+player = Player(config.PLAYER_SIZE, config.PLAYER_X, config.PLAYER_Y, config.PLAYER_SPEED)
+hole = Hole(50, config.WIDTH // 2, config.HEIGHT // 2)
 
 # Основной цикл
 running = True
@@ -33,8 +28,8 @@ while running:
     player.move()
 
     # Ограничение персонажа от выхода за границы экрана
-    player_x = max(0, min(WIDTH - player.size, player.x))
-    player_y = max(0, min(HEIGHT - player.size, player.y))
+    player_x = max(0, min(config.WIDTH - player.size, player.x))
+    player_y = max(0, min(config.HEIGHT - player.size, player.y))
 
     # Проверка на падение в дыру
     distance_to_hole = ((player_x + player.size // 2 - hole.x) ** 2 +
@@ -44,14 +39,14 @@ while running:
         running = False
 
     # Отрисовка
-    window.fill(WHITE)  # Обновление фона
-    pygame.draw.rect(window, BLACK, (player_x, player_y, player.size, player.size))  # Отрисовка персонажа
-    pygame.draw.circle(window, RED, (hole.x, hole.y), hole.radius)  # Отрисовка дыры
+    window.fill(config.WHITE)  # Обновление фона
+    pygame.draw.circle(window, config.RED, (hole.x, hole.y), hole.radius)
+    pygame.draw.rect(window, config.BLACK, (player_x, player_y, player.size, player.size))
 
     pygame.display.flip()  # Обновление окна
 
     # Ограничение частоты кадров
-    pygame.time.Clock().tick(30)
+    pygame.time.Clock().tick(config.FPS)
 
 # Закрытие Pygame
 pygame.quit()
