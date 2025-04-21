c437023540cdf3bef1d9df33ecc5b06beed1dc4f
Vadsxd <vadim.sannikov.2018@mail.ru>
2025-04-21 13:09:00 +0300

    minor item logic add

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 import sys
 
 import pygame
@@ -5,6 +6,7 @@ import pygame
 import config
 from objects.Hole import Hole
 from objects.Player import Player
+from objects.Item import Item
 
 # Инициализация pygame
 pygame.init()
@@ -16,6 +18,7 @@ pygame.display.set_caption("Game with a Hole")
 # Инициализация объектов
 player = Player(config.PLAYER_SIZE, config.PLAYER_X, config.PLAYER_Y, config.PLAYER_SPEED)
 hole = Hole(50, config.WIDTH // 2, config.HEIGHT // 2)
+items = []
 
 # Основной цикл
 running = True
@@ -31,6 +34,14 @@ while running:
     player_x = max(0, min(config.WIDTH - player.size, player.x))
     player_y = max(0, min(config.HEIGHT - player.size, player.y))
 
+    # Спавн вещей
+    # TODO: спавн не в дыре
+    # TODO: скорость спавна вещей
+    # TODO: спавн в дистанции от персонажа
+    if len(items) < 10:
+        item = Item(20, randint(30, config.WIDTH - 40), randint(30, config.HEIGHT - 40), "item")
+        items.append(item)
+
     # Проверка на падение в дыру
     distance_to_hole = ((player_x + player.size // 2 - hole.x) ** 2 +
                         (player_y + player.size // 2 - hole.y) ** 2) ** 0.5
@@ -38,10 +49,20 @@ while running:
         print("Вы провалились в дыру!")
         running = False
 
+    # Проверка на подбор предмета
+    for item in items:
+        distance_to_item = ((player_x + player.size // 2 - item.x) ** 2 +
+                            (player_y + player.size // 2 - item.y) ** 2) ** 0.5
+        if distance_to_item < item.radius:
+            print("Был подобран предмет", item.name)
+            items.remove(item)
+
     # Отрисовка
     window.fill(config.WHITE)  # Обновление фона
     pygame.draw.circle(window, config.RED, (hole.x, hole.y), hole.radius)
     pygame.draw.rect(window, config.BLACK, (player_x, player_y, player.size, player.size))
+    for item in items:
+        pygame.draw.circle(window, config.GREEN, (item.x, item.y), item.radius)
 
     pygame.display.flip()  # Обновление окна
 
---
+++ objects/Item.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, radius, x, y, name):
+        self.radius = radius
+        self.x = x
+        self.y = y
+        self.name = name
